5296d313b0570b325fb457ba633bf01889ca8977
Oleh Toporkov <lezhocheck@Olehs-MacBook-Pro.local>
2022-12-02 13:11:51 +0200

    Added frontend React part

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.scss";
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
